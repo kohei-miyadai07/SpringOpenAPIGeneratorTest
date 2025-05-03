e0ba4b6331eb535c2de9f383a4e308873d8817c6
kohei-miyadai07 <wm25gb87@gmail.com>
2025-05-03 10:34:15 +0900

    feat: プロジェクト作成

---
+++ spring-openapi-generator-test-app/src/main/java/com/miyadai/springopenapigeneratortestapp/SpringOpenapiGeneratorTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.miyadai.springopenapigeneratortestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenapiGeneratorTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenapiGeneratorTestAppApplication.class, args);
+	}
+
+}
---
+++ spring-openapi-generator-test-app/src/test/java/com/miyadai/springopenapigeneratortestapp/SpringOpenapiGeneratorTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miyadai.springopenapigeneratortestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenapiGeneratorTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
